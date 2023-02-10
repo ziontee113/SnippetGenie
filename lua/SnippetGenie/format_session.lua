f4d955bc185a18ba3e5caea514fb9cb6daa7701e
ziontee113 <ziontee113@gmail.com>
2023-02-10 22:13:32 +0700

    feat(fs): mutate_range_with_offet when add_hole()

--- lua/SnippetGenie/format_session.lua
+++ lua/SnippetGenie/format_session.lua
@@ -3,9 +3,11 @@ local lib_selection = require("SnippetGenie.lib.visual_selection")
 local M = {}
 
 M.FormatSession = {
+    original_buffer = nil,
     original_content = nil,
     row_offset = nil,
-    ranges = {},
+
+    holes = {},
 
     left_delimiter = "{",
     right_delimiter = "}",
@@ -15,6 +17,27 @@ M.FormatSession.__index = M.FormatSession
 function M.FormatSession:initiate_original_values()
     self.original_content = lib_selection.get_selection_text()
     self.row_offset = lib_selection.get_visual_range()
+    self.original_buffer = vim.api.nvim_get_current_buf()
+end
+
+local mutate_range_with_offet = function(offset, start_row, start_col, end_row, end_col)
+    end_row = end_row - offset + 1
+    start_row = start_row - offset + 1
+
+    return { start_row, start_col, end_row, end_col }
+end
+
+function M.FormatSession:add_hole()
+    if vim.api.nvim_get_current_buf() == self.original_buffer then
+        local mutated_range =
+            mutate_range_with_offet(self.row_offset, lib_selection.get_visual_range())
+
+        local new_hole = {
+            content = lib_selection.get_selection_text(),
+            range = mutated_range,
+        }
+        table.insert(self.holes, new_hole)
+    end
 end
 
 function M.FormatSession.new()
--- tests/SnippetGenie/format_session_spec.lua
+++ tests/SnippetGenie/format_session_spec.lua
@@ -6,7 +6,7 @@ describe("format_session", function()
         vim.api.nvim_buf_delete(0, { force = true })
     end)
 
-    it("can create format_session", function()
+    it("can initiate new FormatSession instance and add holes to it", function()
         local original_content = [[
 The sun rises in the east.
 I love to drink coffee.
@@ -16,6 +16,7 @@ Welcome Mars. ]]
         helper.set_lines(original_content)
         vim.cmd("norm! 3jVG")
 
+        -- session creation process
         local session = module.FormatSession:new()
 
         local expected_content = [[
@@ -25,5 +26,13 @@ Welcome Mars. ]]
 
         assert.equals(expected_content, session.original_content)
         assert.equals(expected_row_offset, session.row_offset)
+
+        vim.cmd("norm! ")
+
+        -- add_hole process
+        vim.cmd("norm! k0ve")
+        session:add_hole()
+        assert.equals("Hello", session.holes[1].content)
+        assert.are.same({ 1, 1, 1, 5 }, session.holes[1].range)
     end)
 end)
