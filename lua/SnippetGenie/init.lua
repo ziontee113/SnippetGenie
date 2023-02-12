2e37ad96e4739db7bec2f13fb5ba81355c4ad229
ziontee113 <ziontee113@gmail.com>
2023-02-12 19:30:41 +0700

    feat(UAT): detach function contents from mappings

--- lua/SnippetGenie/init.lua
+++ lua/SnippetGenie/init.lua
@@ -4,17 +4,15 @@ local module = require("SnippetGenie.format_session")
 
 local current_session
 
-vim.keymap.set("x", "<CR>", function()
+local create_new_snippet_or_add_placeholder = function()
     if not current_session then
         current_session = module.FormatSession:new()
     else
         current_session:add_hole()
     end
+end
 
-    vim.cmd("norm! ")
-end, {})
-
-vim.keymap.set("n", "<CR>", function()
+local finalize_snippet = function()
     if current_session then
         vim.ui.input({ prompt = "Please enter Trigger for this Snippet" }, function(trigger)
             if trigger then
@@ -23,10 +21,22 @@ vim.keymap.set("n", "<CR>", function()
                 local final_snippet = current_session:produce_final_snippet()
                 N(final_snippet)
 
+                -- TODO: write final_snippet to file
+
                 current_session = nil
             end
         end)
     end
+end
+
+vim.keymap.set("x", "<CR>", function()
+    create_new_snippet_or_add_placeholder()
+
+    vim.cmd("norm! ")
+end, {})
+
+vim.keymap.set("n", "<CR>", function()
+    finalize_snippet()
 end, {})
 
 return M
